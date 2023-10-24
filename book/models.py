60a2772fd93c76e24ff4befbfd9fd565b921593b
kooyoojin <89962183+kooyoojin@users.noreply.github.com>
2023-10-24 15:45:42 +0900

    Update models.py

--- book/models.py
+++ book/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-<<<<<<< HEAD
-from django.contrib.auth.models import User
-=======
->>>>>>> f4ebe818224e47658765983b8c390f51ae5e55a2
+
 
 # Create your models here.
 class Book(models.Model):
@@ -27,20 +24,3 @@ class SubDepartment(models.Model):
 
     def __str__(self):
         return self.name
-<<<<<<< HEAD
-
-class Cart(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-
-
-class BookCart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='book_carts')
-    book = models.ForeignKey('Book', on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.book.title}"
-
-=======
->>>>>>> f4ebe818224e47658765983b8c390f51ae5e55a2
