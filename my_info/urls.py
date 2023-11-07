9097b8de54994b2da9483aab6cee86e02d44726f
yoojin koo <yoodrii@naver.com>
2023-11-07 16:20:09 +0900

    메뉴바 수정

--- my_info/urls.py
+++ my_info/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.my_info, name='my_info'),
     path('/cart', views.cart, name='cart'),
     path('/heart', views.heart, name='heart'),
+
     # path('add_to_cart/<int:book_id>/', views.add_to_cart, name='add_to_cart'),
 ]
+
